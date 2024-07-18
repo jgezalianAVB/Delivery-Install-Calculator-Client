cf9f577bd936d1089819fee904290c774f7feec2
Jack <jack.gezalian@avb.net>
2024-07-18 00:34:27 -0700

    updates

--- helpers.js
+++ helpers.js
@@ -11,8 +11,18 @@ function cleanPrice(price) {
 }
 
 function calculateAverage(service) {
+
+	//price data item example:
+	//{
+	//best_buy_price : "$30 $399+ | $99 <$399",
+	//home_depot_price : "Free",
+	//lowes_price : "$29 $399+ | $79 <$399",
+	//service : "Delivery Charges Updated 3/13/2024"
+	//}
+
 	data = JSON.parse(localStorage.getItem("price_data"));
 	for (let i = 0; i < data.length; i++) {
+		//get correct data element
 		if (data[i].service == service) {
 			let count = 0;
 
@@ -39,9 +49,11 @@ function populateDifference(member_price_id, difference_id, average) {
 	let member_input = document.getElementById(member_price_id);
 	let difference_el = document.getElementById(difference_id);
 
+	//if member inputs 0, difference is just the competitor average
 	if (member_input.value == 0) {
 		difference_el.innerText = "$" + average;
 	}
+	//if member inputs a non-zero value, difference is competitor average - member input
 	if (member_input.value > 0) {
 		difference = average - member_input.value;
 		difference_el.innerText = "$" + difference;
@@ -52,8 +64,10 @@ function populateDifference(member_price_id, difference_id, average) {
 }
 
 function calculateSum() {
+	//get sum_display element defined in index.html
 	const summed_data = document.getElementById("sum_display");
 	let value = 0;
+	//get all difference cells and sum up total, populating summed_data element
 	let difference_tds = document.querySelectorAll(".difference_td");
 	for (let i = 0; i < difference_tds.length; i++) {
 		if (difference_tds[i].innerText) {
@@ -64,10 +78,12 @@ function calculateSum() {
 }
 
 async function storeMemberInputs() {
+	//remove old data
 	if (localStorage.getItem("member_prices") != null) {
 		localStorage.removeItem("member_prices");
 	}
 	const member_input_arr = [];
+	//push all member inputs to local storage
 	inputs = document.querySelectorAll(".member_input");
 	inputs.forEach((element) => {
 		let input_id = element.id;
@@ -78,6 +94,7 @@ async function storeMemberInputs() {
 }
 
 async function storeAnnualizedSum() {
+	//store four piece kitchen annual sum total
 	annualized = document.getElementById("annualized").innerText;
 	localStorage.setItem("four_piece_kitchen_annualized_sum", annualized);
 }
@@ -86,6 +103,7 @@ async function storeTableDeliveryInstall() {
 	let td_arr = [];
 	let td_sub_arr = {};
 	let input_arr = [];
+	//need predefined data keys to rename object keys later
 	let data_keys = [
 		"Service",
 		"Best Buy Price",
@@ -95,11 +113,15 @@ async function storeTableDeliveryInstall() {
 		"Difference",
 	];
 
+	//this stores all table data except headers as formatted JSON
+	//get all table data that is not a header
 	$(document).ready(function () {
 		$(this)
 			.find("td")
 			.not(".header")
+			//push 6 elements to sub array object (as we have 6 columns), then push sub array to td_arr (array of json objects)
 			.each(function (index, element) {
+				//create key:value pair (keys are just sequential numbers at this point)
 				td_sub_arr[index] = element.innerText;
 				if (Object.keys(td_sub_arr).length == 6) {
 					td_arr.push(td_sub_arr);
@@ -107,7 +129,9 @@ async function storeTableDeliveryInstall() {
 				}
 			});
 
+		//get all input values, since above code only finds td elements (14 rows)
 		for (let i = 0; i <= 14; i++) {
+			//if at last row, get placeholder value
 			if (i == 14) {
 				var place_hldr_data = $("#four_piece_delivery").attr("placeholder");
 				input_arr.push(place_hldr_data);
@@ -117,16 +141,23 @@ async function storeTableDeliveryInstall() {
 			input_arr.push(input_data);
 		}
 
+		//for each td_arr object we are missing the member input field as the first .find("td") does not grab input values
+		//populate missing values
+		//for each el of input array (looks like this: [1, 2, 3...])
 		for (let i = 0; i < input_arr.length; i++) {
+			//for each td array element (which are json objects with one val missing: [ {0:"x", 1:"y", 2: ""}, ....])
 			for (let j = 0; j < td_arr.length; j++) {
+				//for each element in object
 				for ([key, val] of Object.entries(td_arr[j])) {
 					if (val == "") {
+						//found key with missing value, set value to same index as td_arr json object (the order maps perfectly as there are 14 inputs and 14 objects)
 						td_arr[j][key] = input_arr[j];
 					}
 				}
 			}
 		}
 
+		
 		function rename(obj, oldName, newName) {
 			if (!obj.hasOwnProperty(oldName)) {
 				return false;
@@ -137,6 +168,7 @@ async function storeTableDeliveryInstall() {
 			return true;
 		}
 
+		//since all keys are just sequential numbers at this point, we need to rename them to the correct service
 		for (sub_arr of td_arr) {
 			Object.keys(sub_arr).forEach(function (el, index, arr) {
 				rename(sub_arr, el, data_keys[index]);
@@ -191,6 +223,7 @@ async function storeTableFourPieceKitchen() {
 			});
 		}
 
+		//need to get data from delta table
 		let delta_text = document.getElementById("delta_cell").innerText
 		let weekly_deliveries = document.getElementById("weekly_deliveries").value
 		let annualized = document.getElementById("annualized").innerText
@@ -209,7 +242,7 @@ async function storeTableFourPieceKitchen() {
 	});
 }
 
-async function storeTableLaundry() {
+function storeTableLaundry() {
 	let td_arr = [];
 	let td_sub_arr = {};
 	let data_keys = [
@@ -250,6 +283,7 @@ async function storeTableLaundry() {
 			});
 		}
 
+
 		let delta_text = document.getElementById("delta_cell").innerText
 		let weekly_deliveries = document.getElementById("weekly_deliveries").value
 		let annualized = document.getElementById("annualized").innerText
@@ -268,32 +302,39 @@ async function storeTableLaundry() {
 		);
 
 	});
+
 }
 
 function downloadStoredTables() {
-	let delivery_install_data = JSON.parse(localStorage.getItem("delivery_install_download_data"))
-	let four_piece_kitchen_data = JSON.parse(localStorage.getItem("four_piece_kitchen_download_data"))
+	//let delivery_install_data = JSON.parse(localStorage.getItem("delivery_install_download_data"))
+	//let four_piece_kitchen_data = JSON.parse(localStorage.getItem("four_piece_kitchen_download_data"))
 	let laundry_data = JSON.parse(localStorage.getItem("laundry_download_data"))
+	console.log(laundry_data)
 
-	let delivery_install_csv = jsonToCsv(delivery_install_data);
-	let four_piece_kitchen_csv = jsonToCsvWithDelta(four_piece_kitchen_data);
+	//let delivery_install_csv = jsonToCsv(delivery_install_data);
+	//let four_piece_kitchen_csv = jsonToCsvWithDelta(four_piece_kitchen_data);
 	let laundry_csv = jsonToCsvWithDelta(laundry_data);
 
-	download(delivery_install_csv, "delivery_install")
-	download(four_piece_kitchen_csv, "four_piece_kitchen")
+
+
+	
+
+	//download(delivery_install_csv, "delivery_install")
+	//download(four_piece_kitchen_csv, "four_piece_kitchen")
 	download(laundry_csv, "laundry");
 
-
+	let total_opportunity = document.getElementById("total_sum").innerText
 	let user_email = document.getElementById("user_email").value
 
-	dataLayer.push({ 
+	dataLayer.push({
 		"event": "download",
 		"user_email": user_email,
-		"total_opportunity" : total_opportunity
+		"total_opportunity": total_opportunity
 	})
 
 }
 
+//https://www.geeksforgeeks.org/how-to-convert-json-object-to-csv-in-javascript/
 function jsonToCsv(jsonData) {
 	let csv = "";
 
@@ -310,6 +351,7 @@ function jsonToCsv(jsonData) {
 	return csv;
 }
 
+//for when there are two sets of headers (extra delta table)
 function jsonToCsvWithDelta(jsonData) {
 	let csv = "";
 
@@ -332,6 +374,7 @@ function jsonToCsvWithDelta(jsonData) {
 	return csv;
 }
 
+//https://www.geeksforgeeks.org/how-to-create-and-download-csv-file-in-javascript/
 const download = (data, name) => {
 	// Create a Blob with the CSV data and type
 	const blob = new Blob([data], { type: "text/csv" });
@@ -351,6 +394,7 @@ const download = (data, name) => {
 };
 
 function fourPieceKitchenValues(table_display_data, member_prices) {
+	//add member prices to display data
 	table_display_data[0].member_price = "$" + member_prices[0].delivery_charges;
 	table_display_data[1].member_price = "$" + member_prices[1].haul_away * 4;
 	table_display_data[2].member_price = "$" + member_prices[13].h2o_hook_up;
@@ -358,6 +402,7 @@ function fourPieceKitchenValues(table_display_data, member_prices) {
 	table_display_data[4].member_price = "$" + member_prices[2].otr_install;
 	table_display_data[5].member_price =
 		"$" +
+		//predefined formula
 		(parseInt(member_prices[3].dw_install) +
 			parseInt(member_prices[12].dw_kit));
 	let sum = 0;
@@ -392,6 +437,7 @@ function laundryValues(table_display_data, member_prices) {
 
 
 function sendEmail() {
+	//using email.js
 	let delivery_install_data = JSON.parse(localStorage.getItem("delivery_install_download_data"))
 	let four_piece_kitchen_data = JSON.parse(localStorage.getItem("four_piece_kitchen_download_data"))
 	let laundry_data = JSON.parse(localStorage.getItem("laundry_download_data"))
@@ -432,10 +478,10 @@ function sendEmail() {
 		);
 
 	let total_opportunity = document.getElementById("total_sum").innerText
-	dataLayer.push({ 
+	dataLayer.push({
 		"event": "email_submitted",
-		"user_email" : user_email,
-		"total_opportunity" : total_opportunity
+		"user_email": user_email,
+		"total_opportunity": total_opportunity
 	})
 }
 
